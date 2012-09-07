e4d49ade722ef6993ef5e7990d388df2cab4ff56
Anton Pleshivtsev <anton@emby.ru>
2012-09-07 21:15:15 +0400

    Url shortener added

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 from base64 import b64decode as decode
-from hashlib import md5
-import os
 import urllib, urllib2
 
 from flask import Flask, request, render_template
+
+from shorten import image_path, path_to_short, short_to_path
+import local
+
 app = Flask(__name__)
 
-current_dir = os.getcwd()
-dest_dir = '%s/storage' % current_dir
-url_mask = 'http://cropme.ru/%s.png'
+dest_dir = local.dest_dir
+url_mask = local.url_mask
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/pencil')
-def pencil():
+@app.route('/pencil/<short_id>')
+def pencil(short_id):
+    path = short_to_path(short_id)
+    if path is None:
+        return ""
     return render_template('pencil.html')
 
 @app.route('/upload', methods=['POST'])
@@ -23,12 +27,26 @@ def upload():
     image = request.form.get('image', None)
     if image is not None:
         raw_image = decode(image)
-        image_name = md5(raw_image).hexdigest()
-        f = open('%s/%s.png' % (dest_dir, image_name, ), 'w')
-        f.write(raw_image)
-        f.close()        
-        return url_mask % image_name
+        path = image_path(raw_image, local.pref_dir)
+        short_id = path_to_short(path)
+        _write('%s/%s' % (dest_dir, path, ), raw_image)
+
+        return url_mask % short_id
     return 'error'
+
+@app.route('/<short_id>')
+def view(short_id):
+    path = short_to_path(short_id)
+    if path is None:
+        return ""
+    return render_template('view.html', **{'path': path})
+
+
+def _write(path, content):
+    f = open(path, 'w')
+    f.write(content)
+    f.close()
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
---
+++ shorten.py
@@ -0,0 +1,29 @@
+from datetime import timedelta, datetime
+from hashlib import md5
+from base_encoder import base_encode as short
+
+from redis import Redis
+
+redis = Redis('localhost')
+
+
+def image_path(raw_image, prefix=''):
+    path = datetime.now().strftime("%y/%m/%d")
+    parts =  path.split("/")
+    parts = map(lambda x: short(int(x)), parts)
+    image_name = md5(raw_image).hexdigest()[:8]
+    return  "%s/%s/%s.png" % (prefix, "/".join(parts), image_name, )
+
+def short_to_path(short_id):
+    path = redis.get(_key(short_id))
+    return path
+
+def path_to_short(path):
+    id = redis.incr(_key("ID"))
+    short_id = short(id)
+    redis.set(_key(short_id), path)
+    return short_id
+
+
+def _key(string):
+    return "cropme:%s" % string
